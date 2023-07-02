eb14cf7a9473613d4a1cbbc47ba81ed76ff4d363
StackJoe <133919453+StackJoe@users.noreply.github.com>
2023-07-02 13:57:56 -0400

    Update: last update wasn't correct

--- java/script.js
+++ java/script.js
@@ -1,11 +1,14 @@
 
 const body = document.querySelector('body');
 const h4 = document.querySelector('h4');
-const cont = document.querySelector('cont');
+const cont = document.querySelector('p');
+
 const modeToggle = document.getElementById('mode-toggle');
 const modeStatus = document.querySelector('.mode-status');
 function toggleMode() {
 body.classList.toggle('dark-mode');
+body.classList.toggle('light-mode');
+body.classList.toggle('default');
 
 const modeMessage = body.classList.contains('dark-mode') ?
     'Dark Mode'
