ee5deb2a81671ed499d741cffe66fe3ec58dd2ae
StackJoe <133919453+StackJoe@users.noreply.github.com>
2023-07-02 13:53:25 -0400

    Update: for toggler

--- java/script.js
+++ java/script.js
@@ -1,18 +1,16 @@
 
-const body = document.querySelector('body');
-const h4 = document.querySelector('h4');
-const cont = document.querySelector('cont');
-const modeToggle = document.getElementById('mode-toggle');
-const modeStatus = document.querySelector('.mode-status');
 function toggleMode() {
-body.classList.toggle('dark-mode');
-
-const modeMessage = body.classList.contains('dark-mode') ?
-    'Dark Mode'
-    : "Light Mode"
-
-modeStatus.innerText = "Currently in " + modeMessage;
-
-  }
-
-modeToggle.addEventListener('click', toggleMode);
+  body.classList.toggle('dark-mode');
+  body.classList.toggle('light-mode');
+  body.classList.toggle('default');
+  
+  const modeMessage = body.classList.contains('dark-mode') ?
+      'Dark Mode'
+      : "Light Mode"
+  
+  modeStatus.innerText = "Currently in " + modeMessage;
+  
+    }
+  
+  modeToggle.addEventListener('click', toggleMode);
+  
